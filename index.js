65da6c5c049b27e59798a3534bfa8e243bb4bec0
Nikolai Kolotilov <x08042@gamail.com>
2022-02-25 00:07:21 +0500

    Сделаны дополнительные задания

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
-const path = require('path');
-const fs = require('fs');
+const path = require('path')
+const fs = require('fs')
 
-const startDir = process.argv[2];
+const startDir = process.argv[2]
+const destinationDirParam = process.argv[3]
 const destinationDir = "./destDir"
-if(!destinationDir) {
-    fs.mkdir("./destDir")
+const keepStartDir = process.argv[4]
+if (destinationDirParam === '0') {
+  fs.mkdirSync(destinationDir)
+}else if(!destinationDirParam){
+  console.log('Specify destination directory or enter "Auto"');
+  return
 }
-  if (!startDir) {
-    console.log('Specify target directory');
-    return;
-  }
+if (!startDir) {
+  console.log('Specify target directory')
+  return
+}
 
+const readDir = (base, level) => {
+  const files = fs.readdirSync(base)
 
-  const readDir = (directory) => {
-    fs.readdir(directory, { withFileTypes: true }, (err, files) => {
-      files.forEach(file => {
-        if (file.isDirectory()) {
-          readDir(path.join(directory, file.name));
-        } else {
-          movingFiles(directory, file.name);
-        }
-      });
-    });
-  };
-  const createDir = (dir, callback) => {
-    fs.stat(dir, (err) => {
-      if (err) {
-        fs.mkdir(dir, () => {
-          if (callback) callback();
-        });
-      } else {
-        if (callback) callback();
-      }
-    });
-  };
+  files.forEach(item => {
+    let localBase = path.join(base, item)
+    let state = fs.statSync(localBase)
+    let fileName = path.basename(item)
+    let dirname = fileName[0].toUpperCase()
+    if (state.isDirectory()) {
+      readDir(localBase, level + 1)
+    } else {
+      createDir(fileName)
+      movingFiles(base, dirname, fileName)
+    }
 
-  const movingFiles = (directory, filename) => {
-    const firstLetter = filename[0].toUpperCase();
-    const newPath = path.join(__dirname, destinationDir, firstLetter);
-    createDir(newPath, () => {
-      fs.link(path.join(directory, filename), path.join(newPath, filename), (err) => {
-        if (err) throw err;
-      });
-    });
-  };
-  const app = () => {
-  createDir(destinationDir);
-  readDir(path.join(__dirname, startDir));
-  fs.rm(startDir, { recursive: true, force: true }, (err) => {
-    if (err) throw err;
   })
-};
-app();
+}
+const createDir = (fileName) => {
+  const firstLetter = fileName[0].toUpperCase()
+  const dirname = `${firstLetter}`
+  const dirPath = path.join(__dirname, destinationDir, dirname)
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath)
+  }
+}
+
+const movingFiles = (dir, dirName, fileName) => {
+  const oldPath = path.join(__dirname, dir, fileName)
+  const newPath = path.join(__dirname, destinationDir, dirName, fileName)
+  fs.linkSync(oldPath, newPath, (err) => {
+    console.log(err)
+  })
+}
+const removeDir = () => {
+  fs.rm(startDir, { recursive: true, force: true }, (err) => {
+    if (err) throw err
+  })
+}
+if (keepStartDir!=='keep'){
+  removeDir()
+}
+readDir(startDir, 0)
